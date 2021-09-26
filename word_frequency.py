455242214a839796760e3e5c4306b299dc602563
student <alanperezse@gmail.com>
2021-09-26 11:51:13 -0600

    Added load_doc_contents method useful for both algorithms. Made algorithm 2 be able to load the contents of a file within the parallel section, thus saving time.

--- word_frequency.py
+++ word_frequency.py
@@ -4,13 +4,9 @@ import pymp
 
 # Given a list of file names, it returns a 2D array of words that were found in
 # the documents (SERIAL)
-def load_docs_words(file_names):
-    rtn = []
-    for file_name in file_names:
-        with open(file_name, 'r') as file:
-            data = file.read()
-            data = data.split()
-            rtn.append(data)
+def load_doc_contents(file_name):
+    with open(file_name, 'r') as file:
+        rtn = file.read().split()
 
     return rtn
 
@@ -27,11 +23,29 @@ def word_frequency(target, words):
 
 # Returns a dictionary containing the frequency of the given words in the given
 # documents (PARALLEL)
+# This algorithm distributes the words among processes
+def targets_frequency_in_docs(target_words, file_names):
+    # Obtain single list of all words in all documents
+    words = []
+    for file_name in file_names:
+        words += load_doc_contents(file_name)
+
+    shared_dict = pymp.shared.dict()
+
+    with pymp.Parallel() as p:
+        # Each processor gets its own target word
+        for target in p.iterate(target_words):
+            print(f'Thread [{p.thread_num} of {p.num_threads}] is evaluating word \'{target}\'')
+            # Check for target word in the list of all words in all documents
+            shared_dict[target] = word_frequency(target, words)
+
+    return shared_dict
+
+
+# Returns a dictionary containing the frequency of the given words in the given
+# documents (PARALLEL)
 # This algorithm distributes the documents among processes
 def targets_frequency_in_docs2(target_words, file_names):
-    # Get 2D array of words per document
-    content_docs = load_docs_words(file_names)
-
     # Set up shared dictionary
     shared_dict = pymp.shared.dict()
 
@@ -42,8 +56,9 @@ def targets_frequency_in_docs2(target_words, file_names):
             shared_dict[target_words[i]] = 0
 
         # Iterate over documents
-        for content_doc in p.iterate(content_docs):
-            print(f'Thread [{p.thread_num} of {p.num_threads}] is evaluating document starting with {content_doc[0]}')
+        for file_name in p.iterate(file_names):
+            print(f'Thread [{p.thread_num} of {p.num_threads}] is evaluating document {file_name}')
+            content_doc = load_doc_contents(file_name)
 
             private_dict = dict()
 
@@ -60,34 +75,30 @@ def targets_frequency_in_docs2(target_words, file_names):
     return shared_dict
 
 
-# Returns a dictionary containing the frequency of the given words in the given
-# documents (PARALLEL)
-# This algorithm distributes the words among processes
-def targets_frequency_in_docs(target_words, file_names):
-    # Obtain single list of all words in all documents
-    words = []
-    for file_name in file_names:
-        with open(file_name, 'r') as file:
-            data = file.read()
-            words += data.split()
-
-    shared_dict = pymp.shared.dict()
-
-    with pymp.Parallel() as p:
-        # Each processor gets its own target word
-        for target in p.iterate(target_words):
-            print(f'Thread [{p.thread_num} of {p.num_threads}] is evaluating word \'{target}\'')
-            # Check for target word in the list of all words in all documents
-            shared_dict[target] = word_frequency(target, words)
-
-    return shared_dict
-
-
 def main():
     # Obtain list of target words
     target_words = ['hate', 'love', 'death', 'night', 'sleep', 'time', 'henry',
         'hamlet', 'you', 'my', 'blood', 'poison', 'macbeth', 'king', 'heart',
         'honest']
+
+    results = {
+        'hate': 332,
+        'love': 3070,
+        'death': 1016,
+        'night': 1402,
+        'sleep': 470,
+        'time': 1806,
+        'henry': 661,
+        'hamlet': 475,
+        'you': 23304,
+        'my': 14203,
+        'blood': 1009,
+        'poison': 139,
+        'macbeth': 288,
+        'king': 4545,
+        'heart': 1458,
+        'honest': 434
+    }
 
     # Obtain list of file names
     file_names = []
@@ -112,7 +123,7 @@ def main():
     # Test results match from algorithm to algorithm and print
     print('Target words frequency:')
     for word, freq in word_count1.items():
-        assert(freq == word_count2[word])   # Assertion Test
+        assert(freq == word_count2[word] == results[word])   # Assertion Test
         print(f'\t{word} - {freq}')
 
     print('All tests passed')
